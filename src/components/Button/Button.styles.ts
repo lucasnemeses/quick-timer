b8b1d10ffd3833cd62b2516e383b7a00c3bc77ea
Lucas Meneses <lucasnemeses01@gmail.com>
2024-06-13 19:23:36 -0300

    config theme

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -17,7 +17,10 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${props => css`
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+
+  /* ${props => css`
     background-color: ${buttonVariants[props.variant]}
-  `}
+  `} */
 `;
